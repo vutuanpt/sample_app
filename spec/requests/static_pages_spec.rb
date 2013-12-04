1bfa6f66b9f88e4f0fddefd1382d367af2407f34
nguyen vu tuan <vutuanpt@gmail.com>
2013-12-04 23:11:53 +0700

    Update help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,5 +22,10 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => "Help")
 		end
+
+		it "should have the title 'Ruby on Rails Tutorial Sample App | Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
 end
