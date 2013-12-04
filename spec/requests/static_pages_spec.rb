133fce038084a1c0ec57e46a48c58133ea9ce4c7
nguyen vu tuan <vutuanpt@gmail.com>
2013-12-04 23:19:36 +0700

    Update about us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,5 +35,10 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => "About Us")
 		end
+
+		it "should have the title 'Ruby on Rails Tutorial Sample App | About Us'" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
 end
