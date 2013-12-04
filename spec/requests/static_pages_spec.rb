6425c6cd14a286ca4fd8eab75f34a06da957a763
nguyen vu tuan <vutuanpt@gmail.com>
2013-12-04 22:47:42 +0700

    Rename application layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => "Sample App")
 		end
+
+		it "should have the title 'Ruby on Rails Tutorial Sample App | Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+		end
+		
 	end
 end
